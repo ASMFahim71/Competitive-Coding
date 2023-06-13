48de6d858404021dce96b3765dcbac1365b5e51f
A S M FAHIM UDDIN <57143881+ASMFahim71@users.noreply.github.com>
2023-06-14 01:51:07 +0600

    Update BigintegerSum.cpp
    more short

--- BigintegerSum.cpp
+++ BigintegerSum.cpp
@@ -1,13 +1,11 @@
 #include<bits/stdc++.h>
 #define ll long long
-#define N 2e5
 using namespace std;
 vector<ll> add(vector<ll>&a,vector<ll>&b)
 {
     
     reverse(a.begin(),a.end());
     reverse(b.begin(),b.end());
-    ll len=min(a.size(),b.size());
     vector<ll> ans;
     ll carry=0;
     for(int i=0;i<a.size();i++)
@@ -15,24 +13,6 @@ vector<ll> add(vector<ll>&a,vector<ll>&b)
         ll val=a[i]+b[i]+carry;
         carry=val/10;
         ans.push_back(val%10);
-    }
-    if(a.size()>len)
-    {
-    for(int i=len;i<a.size();i++)
-    {
-        ll val=a[i]+carry;
-        carry=val/10;
-        ans.push_back(val%10);
-    }
-    }
-    if(b.size()>len)
-    {
-    for(int i=len;i<b.size();i++)
-    {
-        ll val=b[i]+carry;
-        carry=val/10;
-        ans.push_back(val%10);
-    }
     }
     while(carry)
     {
@@ -50,8 +30,9 @@ int main()
     {
         string a1,b1;
         cin>>a1>>b1;
-        vector<ll>a;
-        vector<ll>b;
+        while(a1.size()>b1.size()) b1='0'+b1;
+        while(b1.size()>a1.size()) a1='0'+a1;
+        vector<ll>a,b;
         for(auto x:a1)
         {
             a.push_back(x-'0');
@@ -60,7 +41,6 @@ int main()
         {
             b.push_back(y-'0');
         }
-        cout<<a1<<" + "<<b1<<" = ";
         vector<ll>bigsum = add(a, b);
         for(auto x:bigsum)
         {
